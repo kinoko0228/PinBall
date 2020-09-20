66308155d058d19f3563d24468233e4faa47276b
kinoko0228 <70248669+kinoko0228@users.noreply.github.com>
2020-09-20 19:39:32 +0900

    Initial commit

---
+++ Assets/PointScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointScript : MonoBehaviour
+{
+    private GameObject pointtext;
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.pointtext = GameObject.Find("Point");
+        this.pointtext.GetComponent<Text>().text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+            this.pointtext.GetComponent<Text>().text = score.ToString();
+        }
+        else if (collision.gameObject.tag == "LargeStarTag")
+        {
+            score += 20;
+            this.pointtext.GetComponent<Text>().text = score.ToString();
+        }
+        else if (collision.gameObject.tag == "SmallCloudTag")
+        {
+            score += 30;
+            this.pointtext.GetComponent<Text>().text = score.ToString();
+        }
+        else if (collision.gameObject.tag == "LargeCloudTag")
+        {
+            score += 40;
+            this.pointtext.GetComponent<Text>().text = score.ToString();
+        }
+
+    }
+
+}
